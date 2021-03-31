6d226baa127954b7407b3ce520ad8e57c8317c6d
Senzoda <kassanda.roger@gmail.com>
2021-04-01 00:18:02 +0200

    update

---
+++ HTML/script.js
@@ -0,0 +1,11 @@
+var firstName = "Mac"
+var lastName = "Tyer"
+var username = "Username: " + firstName + " " + lastName;
+console.log(username)
+
+var characterName = "Dr Wolf Gang";
+var character = "Character: " + characterName;
+console.log(character);
+
+document.getElementById("username").innerHTML = username;
+document.getElementById("character").innerHTML = character;
