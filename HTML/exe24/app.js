1505bce03a5eb601a0c24c5bf809ff78f7127690
Senzoda <kassanda.roger@gmail.com>
2021-05-06 16:30:58 +0200

    update

---
+++ HTML/exe24/app.js
@@ -0,0 +1,34 @@
+// assignment JS
+
+var trainingPeriod = [
+
+    {
+        profile: "studentSchool",
+        info: [
+        "Roger",
+        "Kassanda",
+        "Denderleeuw",
+        "Intec",
+        "Brussels",
+        "WebDeveloper",
+        ]
+    },
+    
+    {
+        profile: "stageDuration",
+        info: [
+        "six months",
+        "4 weeks stage",
+        ]
+    },
+    
+    {
+        profile: "stageCompany",
+        info: [
+        "random company",
+        "in Belgium",
+        ]
+    }
+]
+    console.log("Hi my Name is " + trainingPeriod[0].studentSchool[0] + " " + trainingPeriod[0].studentSchool[1] + " studying " + trainingPeriod[0].studentSchool[5] + "@ " + trainingPeriod[0].studentSchool[3] + " in " + trainingPeriod[0].studentSchool[4], " I am planning to do a " + trainingPeriod[1].stageDuration[1] + " in a " + trainingPeriod[2].stageCompany[0] + trainingPeriod[2].stageCompany[1] + ".")
+    
